91da09999ca81ccb8561dcb05eea368d4d26d435
Kushwah Anujsinh <kushwahanuj1974@gmail.com>
2024-04-26 12:51:21 +0530

    shcv

--- src/Components/Catalog/Product/AddtoCart.jsx
+++ src/Components/Catalog/Product/AddtoCart.jsx
@@ -4,6 +4,7 @@ export default function AddtoCart() {
     return (
         <>
             <button className='btn btn-outline-primary btn-sm'>Add to Car2</button>
+            <p> fcvbdyhgv</p>
         </>
     )
 }

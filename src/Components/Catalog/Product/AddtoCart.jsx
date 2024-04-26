9ca517fb747c0e1ebe5f598bccd58e781f5548b5
Kushwah Anujsinh <kushwahanuj1974@gmail.com>
2024-04-26 12:56:20 +0530

    remove add to cart2 to add to cart

--- src/Components/Catalog/Product/AddtoCart.jsx
+++ src/Components/Catalog/Product/AddtoCart.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AddtoCart() {
     return (
         <>
-            <button className='btn btn-outline-primary btn-sm'>Add to Car2</button>
+            <button className='btn btn-outline-primary btn-sm'>Add to Car</button>
         </>
     )
 }

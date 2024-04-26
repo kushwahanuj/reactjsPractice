645805b55f37cb3ea63dceea34e39787bb71246e
Kushwah Anujsinh <kushwahanuj1974@gmail.com>
2024-04-26 12:54:09 +0530

    Revert "shcv"
    This reverts commit 91da09999ca81ccb8561dcb05eea368d4d26d435.

--- src/Components/Catalog/Product/AddtoCart.jsx
+++ src/Components/Catalog/Product/AddtoCart.jsx
@@ -4,7 +4,6 @@ export default function AddtoCart() {
     return (
         <>
             <button className='btn btn-outline-primary btn-sm'>Add to Car2</button>
-            <p> fcvbdyhgv</p>
         </>
     )
 }
